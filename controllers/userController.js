53386f41a5965a32ae8f793df9774b93f9ed9537
Ibtesum Reza Aninda <aninda7152@gmail.com>
2023-03-06 03:53:52 +0600

    UserAuth, controller, middleware and routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,4 +24,58 @@ const authUser = AsyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// @desc    Register a new user
+// @route   POST /api/users
+// @access Public
+const registerUser = AsyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data");
+  }
+});
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = AsyncHandler(async (req, res) => {
+  // res.send("success")
+
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authUser, registerUser, getUserProfile };
