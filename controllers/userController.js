d8e1263a305b8e127b1b49e609a210b7ccc9d51b
Ibtesum Reza Aninda <aninda7152@gmail.com>
2023-05-26 20:03:28 +0600

    deleteUser Api done

--- controllers/userController.js
+++ controllers/userController.js
@@ -118,5 +118,23 @@ const getUsers = AsyncHandler(async (req, res) => {
 });
 
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers };
+// @desc    Delete Users
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+const deleteUser = AsyncHandler(async (req, res) => {
+  // res.send("success")
+
+  const user = await User.findById(req.params.id);
+  if(user) {
+    await user.remove()
+    res.json({message: 'User removed.'})
+  }else {
+    res.status(404)
+    throw new Error("User not found!")
+  }
+
+});
+
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser };
 
